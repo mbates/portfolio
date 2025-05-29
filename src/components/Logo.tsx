f2ef9be00ad3e3339a8b96269c237a6b94210761
Mike Bates <mike@bates-solutions.com>
2025-05-29 00:31:12 -0700

    style: full width header and logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import './Logo.scss';
 
 const Logo: React.FC = () => {
   return (
-    <div className='logo m-0 relative box-content text-purple-600 border-10 before:absolute  before:bg-white'>
+    <div className='logo m-0 relative box-content text-purple-600 border-13 before:absolute  before:bg-white'>
       <span className='box-1 absolute bg-purple-600' />
       <span className='box-2 absolute bg-purple-600' />
       <span className='box-3 absolute bg-purple-600' />
