bca96fa5165ca79946f1d3b9394801677bc1f83c
Mike Bates <mike@bates-solutions.com>
2025-06-06 03:05:16 -0700

    feat: remove fluff language

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,30 +2,11 @@ import { Link } from 'react-router-dom';
 
 const About: React.FC = () => {
   return (
-    <div className='w-f h-full'>
+    <div className='w-f'>
       <p className='my-5'>
-        At Bates Solutions, we are a forward-thinking software development
-        company dedicated to building innovative, scalable, and high-performing
-        digital solutions. Founded on the principles of excellence, agility, and
-        client collaboration, we specialize in custom software development,
-        mobile and web applications, cloud integration, and enterprise system
-        solutions.
+        Based in Vancouver, we have delivered solutions used across Canada and
+        the US.
       </p>
-      <p className='my-5'>
-        We focus on flexibility with our projects, ensuring that we deliver
-        solutions that fit your budget.
-      </p>
-      <p className='my-5'>
-        Whether you need to modernize your legacy systems, develop a new digital
-        platform, or augment your in-house team, we provide end-to-end software
-        development services tailored to your business goals.
-      </p>
-      <p className='my-5'>
-        Bates Solutions is based in Vancouver and has delivered solutions used
-        across Canada and the US.
-      </p>
-
-      <h2 className='text-3xl font-semibold tracking-tight my-5'>Services</h2>
 
       <h3 className='text-2xl font-semibold tracking-tight my-3'>
         Custom Software Development
